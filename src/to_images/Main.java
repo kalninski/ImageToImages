6f3a6792572c6cb954dc895202b318e675d109ed
Toms <tomkalninsky@gmail.com>
2025-12-07 11:09:29 +0200

    Larry

--- src/to_images/Main.java
+++ src/to_images/Main.java
@@ -1,12 +1,17 @@
 package to_images;
 
 import java.awt.image.*;
+import java.time.Clock;
 
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		Clock clock = Clock.systemDefaultZone();
+		
+		long start = clock.millis();
 		
 	//	ImageNode n1 = new ImageNode("C:\\Users\\Toms\\Desktop\\Foto_Prezidents_Edgars_Rinkēvičs\\Foto_Prezidents_Edgars_Rinke╠ävic╠īs\\New folder\\Ziema╠ä vajag sniegu!.jpg");
 		
@@ -20,13 +25,16 @@ public class Main {
 		
 	//	ic.sort((ImageNode n1, ImageNode n2) -> n1.avgCol > n2.avgCol);
 
-//		ImageToEdit iTE = new ImageToEdit("C:\\Users\\Toms\\Downloads\\118-1188844_thinking-emoji-meme-png-transparent-png_upscayl_4x_ultramix-balanced-4x.png");
-		ImageToEdit iTE1 = new ImageToEdit("C:\\Users\\Toms\\Desktop\\ASCII_IMAGES\\ES_SMAIDU_upscayl_2x_ultramix-balanced-4x.png");
-		iTE1.imageCollection = ic;
+		ImageToEdit iTE = new ImageToEdit("C:\\Users\\Toms\\Downloads\\Larry_le_malicieux_Upscaled_4x.png");
+	//	ImageToEdit iTE1 = new ImageToEdit("C:\\Users\\Toms\\Downloads\\Larry_small_upscayl_4x_ultramix-balanced-4x.png");
+	//	iTE1.imageCollection = ic;
+		iTE.imageCollection = ic;
 	//	iTE.oneStrip(iTE.listStripsSource.get(0), 0);
 		
-	//	iTE.renderImage("EMOJI", "png");
-		iTE1.renderImage("ES_Smaidu_gramatas", "png");
+		iTE.renderImage("LARRY_LE_MALICIEUX", "png");
+//		iTE1.renderImage("Larriy_larry", "png");
+		long end = clock.millis();
+		System.out.println("Time it took to render = " + (end - start) + " milliseconds");
 	}
 
 }
